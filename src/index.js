8063d72ecc90e5a42475d82031e07d529053e635
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-05-24 03:39:12 -0700

    design with webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import _ from 'lodash'
 import './style.css';
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+const component = () => {
+  
 }
 
 document.body.appendChild(component());
